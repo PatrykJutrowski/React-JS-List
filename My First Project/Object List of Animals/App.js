abf4cb726998e00c00dc5ed1f525960eb4e27c47
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2023-01-25 20:30:02 +0100

    Add files via upload

---
+++ My First Project/Object List of Animals/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ObjectList from "./ObjectList/ObjectList";
+
+function App() {
+    return (
+        <div className="App">
+            <ObjectList/>
+        </div>
+    );
+}
+
+export default App;
